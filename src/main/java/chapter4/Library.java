83a93d158c20b290b4a5c782ea01d5b5db8339f7
nsirtak <nsirtak@gmail.com>
2024-11-03 13:12:11 -0600

    added solutions to chapter 4

--- src/main/java/chapter4/Library.java
+++ src/main/java/chapter4/Library.java
@@ -1,15 +1,23 @@
 package chapter4;
 
+import java.util.ArrayList;
+
 public class Library {
 
-    // TODO: Declare a field, books, as an ArrayList of Book objects
-    // Give it private access
+    private ArrayList<Book> books = new ArrayList<>();
 
     // Note: the default constructor is sufficient to instantiate a library object
 
-    // TODO: Define a public method, addBook()
+    public void addBook(Book book) {
+        books.add(book);
+    }
 
-    // TODO: Define a public method, printAvailableBooks()
-    // It should print only books that aren't checked out
-
+    public void printAvailableBooks() {
+        System.out.println("\nLIST OF BOOKS AVAILABLE:");
+        for (Book book : books) {
+            if (book.isAvailable()) {
+                System.out.println("\t" + book.getTitleAndAuthor());
+            }
+        }
+    }
 }
